69dd58f9c2e796005011b32f6903bb1aad8e0347
monirhossain-oss <mdmonirhos571@gmail.com>
2025-09-15 23:37:59 +0600

    added a responsive nevber

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>This is Navbar</div>;
+  return <div>
+    <h2>This is Navbar</h2>
+    <h3>This is hero section than others</h3>
+  </div>;
 };
 
 export default Navbar;
