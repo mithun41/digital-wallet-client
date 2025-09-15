d49c25ebc34f58541368c4792ab9ddb90b4d2c53
mhsumon0174 <mhsumon0174@gmail.com>
2025-09-15 19:52:05 +0600

    environment-set-up

---
+++ src/Pages/Home/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>This Component is added by MH Sumon</h1>
+            <h2>Content will be added here soon</h2>
+        </div>
+    );
+};
+
+export default Blog;
