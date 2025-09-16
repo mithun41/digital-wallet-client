d681d6974be551661d8635a87de7dce4cf762fb7
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2025-09-16 19:49:29 +0600

    added login reg

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Navbar = () => {
-  return <div>
-    <h2>This is Navbar</h2>
-    <h3>This is hero section than others</h3>
-  </div>;
+
+  return (
+    <div className="my-4 flex justify-around">
+
+      <Link to='/' className=" text-black m-2 px-2"> Home</Link>
+      <Link to='/login' className="bg-amber-300 text-black m-2 px-2"> login</Link>
+      <Link to='/Register' className="bg-amber-300 text-black m-2 px-2"> Register</Link>
+  </div>
+  )
 };
 
 export default Navbar;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
-    children: [{ index: true, Component: Home }],
+    children: [
+      { 
+        index: true, Component: Home
+      },
+      {
+        path:'/login',
+        Component: Login
+      },
+      {
+        path:'/Register',
+        Component: Register
+      }
+
+    ],
   },
 ]);
