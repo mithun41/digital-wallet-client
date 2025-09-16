a8ee3c3448c46a8e7030a24026591453053c5a97
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2025-09-16 19:56:48 +0600

    login * reg

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 const Navbar = () => {
 
   return (
-    <div className="my-4 flex justify-around">
+    <div className="my-4 flex">
 
       <Link to='/' className=" text-black m-2 px-2"> Home</Link>
       <Link to='/login' className="bg-amber-300 text-black m-2 px-2"> login</Link>
