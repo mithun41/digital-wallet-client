078bac9571946cef0c25687ba440798a704c0a57
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2025-09-16 19:51:21 +0600

    Revert "added login reg"

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Link } from "react-router";
 
 const Navbar = () => {
-
-  return (
-    <div className="my-4 flex justify-around">
-
-      <Link to='/' className=" text-black m-2 px-2"> Home</Link>
-      <Link to='/login' className="bg-amber-300 text-black m-2 px-2"> login</Link>
-      <Link to='/Register' className="bg-amber-300 text-black m-2 px-2"> Register</Link>
-  </div>
-  )
+  return <div>
+    <h2>This is Navbar</h2>
+    <h3>This is hero section than others</h3>
+  </div>;
 };
 
 export default Navbar;
