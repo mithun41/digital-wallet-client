e7c0d99ef7c07fadebe4bc559a694712a2a4311f
mithun8441 <mahmudulhasan3527@gmail.com>
2025-09-17 21:38:28 +0600

    test

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      This is Home
-      <h3>Mithun</h3>
-      <p>hello</p>
-      <p>Mithun</p>
-      <p>my name is Sumaiya .....</p>
-      
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
