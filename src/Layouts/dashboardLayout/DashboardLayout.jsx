2a8d43b92e15d94a0f00515a04c0a1740ac11f50
shiuly-28 <shulybd1245@gmail.com>
2025-09-25 11:15:57 +0600

    improve Cashout page code structure

--- src/Layouts/dashboardLayout/DashboardLayout.jsx
+++ src/Layouts/dashboardLayout/DashboardLayout.jsx
@@ -60,12 +60,12 @@ const DashboardLayout = () => {
       {/* mobile */}
       <div>
         <div onClick={handleMenuBar}
-          >
-            {
-              menuBar ? <ImCross className="fixed top-20 cursor-pointer opacity-50 hover:opacity-100 transition-all duration-500 ease-linear bg-[#155DFC] h-16 w-16 ml-10 p-3 rounded-full left-2 z-50 flex lg:hidden" /> : <CiMenuBurger className="fixed top-20 cursor-pointer opacity-50 hover:opacity-100 transition-all duration-500 ease-linear bg-[#155DFC] h-16 w-16 ml-10 p-3 rounded-full left-2 z-50 flex lg:hidden" />
-            }
-          
-          
+        >
+          {
+            menuBar ? <ImCross className="fixed top-20 cursor-pointer opacity-50 hover:opacity-100 transition-all duration-500 ease-linear bg-[#155DFC] h-16 w-16 ml-10 p-3 rounded-full left-2 z-50 flex lg:hidden" /> : <CiMenuBurger className="fixed top-20 cursor-pointer opacity-50 hover:opacity-100 transition-all duration-500 ease-linear bg-[#155DFC] h-16 w-16 ml-10 p-3 rounded-full left-2 z-50 flex lg:hidden" />
+          }
+
+
         </div>
         <div
           className={`lg:hidden  z-10   fixed bg-gray-700/50 backdrop-blur p-5 rounded-2xl  flex flex-col gap-5     transition-all duration-300     ${menuBar ? "left-10 top-38 opacity-100" : "-left-20 top-38 opacity-0"}  `}
@@ -88,19 +88,19 @@ const DashboardLayout = () => {
           >
             Profile
           </Link>
-           <Link
-          to="/dashboard/AddMoney"
-          className="hover:text-gray-200 px-5 py-1 bg-[#155DFC] rounded block"
-        >
-          Add Money
-        </Link>
+          <Link
+            to="/dashboard/AddMoney"
+            className="hover:text-gray-200 px-5 py-1 bg-[#155DFC] rounded block"
+          >
+            Add Money
+          </Link>
 
-        <Link
-          to="/dashboard/MobileRecharge"
-          className="hover:text-gray-200 px-5 py-1 bg-[#155DFC] rounded block"
-        >
-          Mobile Recharge
-        </Link>
+          <Link
+            to="/dashboard/MobileRecharge"
+            className="hover:text-gray-200 px-5 py-1 bg-[#155DFC] rounded block"
+          >
+            Mobile Recharge
+          </Link>
           <Link
             to="/dashboard/cashOut"
             className="hover:text-gray-200 px-5 py-1 bg-[#155DFC] rounded block"
@@ -112,7 +112,7 @@ const DashboardLayout = () => {
           </button>
         </div>
       </div>
-      <div className="flex-1 flex items-center justify-center mt-8 w-full">
+      <div className="flex-1 flex items-center justify-center mt-8 ">
         <Outlet></Outlet>
       </div>
     </div>
