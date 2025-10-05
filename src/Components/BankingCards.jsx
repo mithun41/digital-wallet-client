1c66e2f350228cbb7631f781c0e5399efe878d1e
shiuly-28 <shulybd1245@gmail.com>
2025-10-05 10:36:18 +0600

    updated the bankingCard new image

--- src/Components/BankingCards.jsx
+++ src/Components/BankingCards.jsx
@@ -12,7 +12,7 @@ const BankingCards = () => {
       id: 1,
       title: "Online Banking For Small Businesses",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      img: "https://i.postimg.cc/SR34fZj4/images.jpg",
+      img: "https://i.postimg.cc/qqjdc9pB/Screenshot-2.png",
     },
     {
       id: 2,
@@ -24,71 +24,76 @@ const BankingCards = () => {
       id: 3,
       title: "Online Banking For Small Businesses",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      img: "https://i.postimg.cc/qqjdc9pB/Screenshot-2.png",
+      img: "https://i.postimg.cc/W1q78vTH/Screenshot-1.png",
     },
     {
       id: 4,
       title: "Online Banking For Small Businesses",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      img: "https://i.ibb.co.com/fqZp0mC/image4.jpg",
+      img: "https://i.postimg.cc/0N60tqvZ/Screenshot-3.png",
     },
   ];
 
   return (
-    <div className=" flex items-center max-w-11/12 mx-auto justify-center px-6 py-12">
-      <div className="flex  ">
-        <div className="border border-amber-400">
+   <div className="mt-8">
+    <div className="text-center ">
+        <h1 className="text-3xl font-bold text-green-400">Latest news & Events</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      </div>
+     <div className=" flex items-center max-w-11/12 mx-auto justify-center px-6  py-12">
+      
+      <div className="flex gap-4 ">
+        <div className="gap-4 ">
            {/* Row 1 */}
-       <div className="flex">
+       <div className="flex gap-4 ">
          <div
-          className="col-span-2 md:col-span-2 flex flex-col md:flex-row bg-black/40 border border-purple-500/40 rounded-2xl overflow-hidden"
+          className="col-span-2 md:col-span-2 flex flex-col md:flex-row border border-green-500/40 h-100 rounded-2xl overflow-hidden"
           data-aos="fade-right"
         >
           <img
             src={cards[0].img}
             alt="card"
-            className="w-full md:w-1/2 object-cover"
+            className="w-full md:w-1/2  "
           />
           <div className="p-6 flex flex-col justify-center">
-            <h2 className="text-lg font-bold text-white">{cards[0].title}</h2>
-            <p className="text-gray-300 text-sm mt-2">{cards[0].desc}</p>
-            <button className="text-white mt-4 font-semibold hover:underline">
+            <h2 className="text-lg font-bold text-secondary">{cards[0].title}</h2>
+            <p className="text-secondary text-sm mt-2">{cards[0].desc}</p>
+            <button className="text-secondary mt-4 font-semibold hover:underline">
               Get Started
             </button>
           </div>
         </div>
         {/* ------ */}
          <div
-          className="col-span-2 flex flex-col md:flex-row-2 bg-black/40 border border-purple-500/40 rounded-2xl overflow-hidden"
+          className="col-span-2 p-6  flex-col justify-center flex flex-col md:flex-row-2  border border-green-500/40 gap-5 rounded-2xl overflow-hidden"
           data-aos="fade-left"
         >
          
-          <div className="p-6 flex flex-col justify-center">
-            <h2 className="text-lg font-bold text-white">{cards[3].title}</h2>
-            <p className="text-gray-300 text-sm mt-2">{cards[3].desc}</p>
-            <button className="text-white mt-4 font-semibold hover:underline">
+          <div className="">
+            <h2 className="text-lg font-bold text-secondary">{cards[3].title}</h2>
+            <p className="text-secondary text-sm mt-2">{cards[3].desc}</p>
+            <button className="text-secondary mt-4 font-semibold hover:underline">
               Get Started
             </button>
           </div>
         </div> 
        </div>
         {/* row column */}
-
         <div
-          className="flex bg-black/40 border border-purple-500/40 rounded-2xl p-6 justify-center"
+          className="flex border border-green-500/40 rounded-2xl mt-4 justify-center"
           data-aos="fade-up"
         >
        <div>
             <img
             src={cards[3].img}
             alt="card"
-            className=" md:w-1/2 object-cover"
+            className="w-full object-cover"
           />
        </div>
-         <div>
-           <h2 className="text-lg font-bold text-white">{cards[1].title}</h2>
-          <p className="text-gray-300 text-sm mt-2">{cards[1].desc}</p>
-          <button className="text-white mt-4 font-semibold hover:underline">
+         <div className="p-6 flex flex-col justify-center">
+           <h2 className="text-lg font-bold text-secondary">{cards[1].title}</h2>
+          <p className="text-secondary text-sm mt-2">{cards[1].desc}</p>
+          <button className="text-secondary mt-4 font-semibold hover:underline">
             Get Started
           </button>
          </div>
@@ -97,7 +102,7 @@ const BankingCards = () => {
        
 
         <div
-          className="bg-black/40 border border-purple-500/40 rounded-2xl overflow-hidden"
+          className="border border-green-500/40 rounded-2xl overflow-hidden"
           data-aos="zoom-in"
         >
           <img
@@ -105,14 +110,19 @@ const BankingCards = () => {
             alt="card"
             className="w-full  object-cover"
           />
+          <div className="p-6 mt-5">
+           <h2 className="text-lg font-bold text-secondary">{cards[1].title}</h2>
+          <p className="text-secondary text-sm mt-2">{cards[1].desc}</p>
+          <button className="text-secondary mt-4 font-semibold hover:underline">
+            Get Started
+          </button>
+         </div>
         </div> 
 
         {/* Row 2 */}
-        
-
-     
-      </div>
+        </div>
     </div>
+   </div>
   );
 };
 
